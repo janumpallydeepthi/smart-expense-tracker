e26dda9f862d6a84e72ff5ccb5a05c58f32949bc
janumpallydeepthi <deepthi.janumpally@gmail.com>
2026-06-29 23:19:06 +0530

    hardcoded api.js changed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/api", // ⚠️ make sure this matches your backend port
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3001/api",
 });
 
 // Attach JWT token automatically
